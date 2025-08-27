75fd8802879ea18642f28f8216dba0985bfb521a
BoubacarSIDDY <dialloboubacarsiddy2004@gmail.com>
2025-08-27 22:04:09 +0000

    retrieving variables in the view

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 class HomeController extends Controller
 {
     public function index(){
-        return view('home');
+        return view('home', ['message' => 'Bienvenue sur framework-gaw !']);
     }
 
     public function contact(){
